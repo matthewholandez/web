3420aba1644a39c166687891fb67e048f73b921b
Matthew Holandez <91989998+matthewholandez@users.noreply.github.com>
2026-07-29 17:21:50 -0400

    update time and now

--- app/time/config.ts
+++ app/time/config.ts
@@ -18,6 +18,11 @@ export const EVENTS: CountdownEvent[] = [
     targetLabel: "August 5, 2026 · 5:00 PM ET",
     eventLabel: "End of 1B term at Waterloo",
   },
+    {
+    target: new Date("2026-09-08T13:00:00Z"),
+    targetLabel: "September 8, 2026 · 9:00 AM ET",
+    eventLabel: "Start of 2nd co-op",
+  },
 ];
 
 /**
